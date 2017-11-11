d25c07c4e6bdbc1b50598afe231098ac37aff1ca
tycrawford <tycrawford314@gmail.com>
2017-11-10 18:23:06 -0600

    added templates, models, hash utilities,

--- main.py
+++ main.py
@@ -1,2 +1,50 @@
 from flask import Flask
+from flask import request, redirect, render_template, session, flash
+import cgi
 from app import app, db
+from models import User, Game, Solution, Turn, Room, NoteCard, Log, Character
+from hashutils import checkPwHash
+
+defaultLoginHeader = """
+
+""" #TODO consider entering a basic form here that will redirect to hte login route with the appropriate post method to trigger a login function, rhather than force the user to go straight to a login page to login
+ 
+userLoggedInHeader = """
+Signed in as {0} LOGOUT
+""" #TODO enter a proper href for the logout
+
+@app.route("/")
+def homepage():
+    #TODO enter code that looks for cookies to see if the user is logged in, turn into IF statement returning base.html with different loginHeaders
+    return render_template('base.html')
+
+@app.route("/login")
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')            #Allows for just the basic login screen
+    else:
+        username = request.form['username']             #Gathers posted usernamne
+        password = request.form['password']             #Gathers posted password
+        users = User.query.filter_by(username=username) #compares username against usernames in database
+        if users.count() == 1:                          #If there is a match (should only be one, usernames are unique)
+            user = users.first()                        #Pull the first (and only) resulting object
+            if checkPwHash(password, user.pwHash):      #If the password entered hashes properly and produces the hashed database password
+                session['user'] = user.username         #sets the sessions user to the username of the user object
+                return redirect("/")                    #brings us back to the homepage
+            else:
+                return render_template("/login", passwordError="Bad Password") #returns the appropriate bad password error
+        else:
+            return render_template("/login", usernameError="Bad Username") #returns bad username error
+
+@app.route("/mygames")
+def mygames():
+    #TODO enter code that queries the databse for a list of all games belonging to the player attributed to teh signed in user. 
+    #TODO find a way to format them into a jiinja template
+    #TODO return in the form of a table, show basic information, player username unordered list, game status, current player turn, etc. 
+    
+
+@app.route("/gamenumber")
+def gamenumber():
+    #TODO enter code that pulls all relevant data on the game passed in the URL
+    #TODO format it into blocks to send to a jinja template
+
--- models.py
+++ models.py
@@ -1,23 +1,131 @@
 from app import db
-#from hashutils import make_pw_hash
+from hashutils import makePwHash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
-    pw_hash = db.Column(db.String(120))
+    pwHash = db.Column(db.String(120))
     #games = db.relationship('Games', backref='' maybe connect with character, which has a game id column instead of games
-
     gamesPlayed = db.Column(db.Integer)
     score = db.Column(db.Integer)
 
+    def __init__(self, username, password):
+        self.username = username
+        self.pwHash = makePwHash(password)
+
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    scarlet = db.Column(db.Integer,)
+    scarlet = db.Column(db.Integer)
+
+    def __init__(self, playerID, playerChar):
+        if playerChar == "Scarlet":
+            self.Scarlet = playerID
+        elif playerChar == "White":
+            self.White = playerID
+        elif playerChar == "Mustard":
+            self.Mustard = playerID
+        elif playerChar == "Green":
+            self.Green = playerID
+        elif playerChar == "Peacock":
+            self.Peacock = playerID
+        elif playerChar == "Plum":
+            self.Plum = playerID
 
 
 class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    gameID = db.Column()
-    userID = db.Column()
+    gameID = db.Column(db.Integer, ForeignKey('Game.id'))
+    userID = db.Column(db.Integer, ForeignKey('User.id'))
+    character = db.Column(db.String(20))
+    hand = db.Column(db.String(50))
+    position = db.Column(db.String(12))
+    pter = db.Column(db.String(20))
+    room = db.Column(db.String(20))
+    notecard = db.Column(db.Integer, ForeignKey('notecard.id'))
 
+    def __init__(self, gameID, userID, character):
+        self.gameID = gameID
+        self.userID = userID
+        self.character = character
 
+    
+class Solution(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gameID = db.Column(db.Integer, ForeignKey('Game.id'))
+    suspect = db.Column(db.Integer)
+    weapon = db.Column(db.Integer)
+    room = db.Column(db.Integer)
+
+    def __init__(self, suspect, weapon, room):
+        self.suspect = suspect
+        self.weapon = weapon
+        self.room = room
+        
+class Turn(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gameID = db.Column(db.Integer, ForeignKey('Game.id'))
+    turnNumber = db.Column(db.Integer)
+    character = db.Column(db.String(20))
+    dieRoll = db.Column(db.Integer)
+    diceRolled = db.Column(db.Boolean)
+    moveMade = db.Column(db.Boolean)
+    pter = db.Column(db.String(20))
+    room = db.Column(db.String(20))
+    suggestionMade = db.Column(db.Boolean)
+    turnOver = db.Column(db.Boolean)
+    suggestion = db.Column(db.String(15))
+    
+    def __init__(self, gameID, turnNumber, pter, room, character):
+        self.gameID = gameID
+        self.turnNumber = turnNumber
+        self.pter = pter
+        self.character = character
+        self.room = room
+        self.dieRoll = 0
+        self.suggestionMade = False
+        self.turnOVer = False
+        self.moveMade = False
+        self.diceRolled = False
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gameID = db.Column(db.Integer)
+    name = db.Column(db.String(20))
+    spaces = db.Column(db.String(200))
+    characters = db.Column(db.String(30))
+    spRoom = db.Column(db.Integer)
+    doorSpaces = db.Column(db.String(30))
+
+    def __init__(self, gameID, name, spaces, spRoom, doorSpaces):
+        self.gameID = gameID
+        self.name = name
+        self.spaces = spaces
+        self.spRoom = spRoom
+        self.doorSpaces = doorSpaces
+        
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gameID = db.Column(db.Integer)
+    publicString = db.Column(db.String(200))
+    privateString = db.Column(db.String(200))
+    privUser = db.Column(db.String(20))
+    timeStamp = db.Column(db.String(30))
+    
+    def __init__(self, gameID, publicString, privateString, privUser, timeStamp):
+        self.gameID = gameID
+        self.publicString = publicString
+        self.privateString = privateString
+        self.privUser = privUser
+        self.timeStamp = timeStamp
+    
+class NoteCard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    suspect = db.Column(db.String(200))
+    weapon = db.Column(db.String(200))
+    room = db.Column(db.String(200))
+    
+    def __init__(self, suspect, weapon, room):
+        self.suspect = suspect
+        self.weapon = weapon
+        self.room = room
+        
